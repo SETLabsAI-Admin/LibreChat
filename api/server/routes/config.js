5697be5c756d5fa0c8e206458df0a3a554358c6c
Jeffrey Fougere <jeffreyfougere@Jeffreys-Mac-Studio.local>
2025-08-15 20:48:17 -0500

    feat: Update branding to AskHenry

--- api/server/routes/config.js
+++ api/server/routes/config.js
@@ -54,7 +54,7 @@ router.get('/', async function (req, res) {
 
     /** @type {TStartupConfig} */
     const payload = {
-      appTitle: 'AskHenry',// process.env.APP_TITLE || 'LibreChat', 
+      appTitle: process.env.APP_TITLE || 'Chat', 
       socialLogins: req.app.locals.socialLogins ?? defaultSocialLogins,
       discordLoginEnabled: !!process.env.DISCORD_CLIENT_ID && !!process.env.DISCORD_CLIENT_SECRET,
       facebookLoginEnabled:
